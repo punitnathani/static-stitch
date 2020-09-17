12894eeb25cf7a5971b0b90205f64fd005e5c90c
Punit Nathani <corporate2punit@live.com>
2020-09-17 15:31:01 +0530

    Add responsive menu with auto-generated links

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = (function(eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
   eleventyConfig.addPassthroughCopy('src/js');
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('src/assets');
